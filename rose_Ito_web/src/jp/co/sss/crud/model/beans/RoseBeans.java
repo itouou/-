bd4106441ae789478f5f221b0d0cc8a7f33714bf
itouou <shotanagaizumi@gmail.com>
2020-11-05 20:50:52 +0900

    追加

--- rose_Ito_web/src/jp/co/sss/crud/model/beans/RoseBeans.java
+++ rose_Ito_web/src/jp/co/sss/crud/model/beans/RoseBeans.java
@@ -2,4 +2,42 @@ package jp.co.sss.crud.model.beans;
 
 public class RoseBeans {
 
+	private int roseId;
+	private String roseName;
+	private int rosePrice;
+	private String rosePicture;
+
+
+	public int getRoseId() {
+		return roseId;
+	}
+
+	public void setRoseId(int roseId) {
+		this.roseId = roseId;
+	}
+
+	public String getRoseName() {
+		return roseName;
+	}
+
+	public void setRoseName(String roseName) {
+		this.roseName = roseName;
+	}
+
+	public int getRosePrice() {
+		return rosePrice;
+	}
+
+	public void setRosePrice(int rosePrice) {
+		this.rosePrice = rosePrice;
+	}
+
+	public String getRosePicture() {
+		return rosePicture;
+	}
+
+	public void setRosePicture(String rosePicture) {
+		this.rosePicture = rosePicture;
+	}
+
 }
--- rose_Ito_web/src/jp/co/sss/crud/model/daos/RoseDao.java
+++ rose_Ito_web/src/jp/co/sss/crud/model/daos/RoseDao.java
@@ -1,17 +1,52 @@
 package jp.co.sss.crud.model.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import jp.co.sss.crud.model.beans.RoseBeans;
 
 public class RoseDao {
 
 	public static RoseBeans selectAll() {
 
-		RoseBeans rb = null;
+		Connection con =null;
+		PreparedStatement ps = null;
+		RoseBeans rosebeans = null;
 
-		//接続とselect文とそれをRoseBeansに保持するところ
+		try {
+			con = DBManager.getConnection();
 
-		return rb;
+			// SQL文を準備
+			String sql = "";
 
+			ps = con.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+
+			// 検索結果をbeanに格納する
+			while (rs.next()) {
+				rosebeans = new RoseBeans();
+				rosebeans.setRoseId(rs.getInt("RoseID"));
+				rosebeans.setRoseName(rs.getString("RoseName"));
+				rosebeans.setRosePicture(rs.getString("RosePicture"));
+				rosebeans.setRosePrice(rs.getInt("price"));
+
+
+				//RoseBeans rb = null;
+
+				//接続とselect文とそれをRoseBeansに保持すると
+			}
+		} catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // DB切断処理
+            DBManager.close(ps, con);
+        }
+		return rosebeans;
+
+		}
 	}
 
-}
+
