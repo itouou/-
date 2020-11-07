31101282b516c74cbf2f83253c38dd0f08fb4bc7
itouou <shotanagaizumi@gmail.com>
2020-11-07 14:38:54 +0900

    修正

--- rose_Ito_web/src/jp/co/sss/crud/model/daos/RoseDao.java
+++ rose_Ito_web/src/jp/co/sss/crud/model/daos/RoseDao.java
@@ -4,12 +4,55 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import jp.co.sss.crud.model.beans.RoseBeans;
 
 public class RoseDao {
 
-	public static RoseBeans selectAll() {
+	public static List<RoseBeans> selectAll() {
+
+		Connection con =null;
+		PreparedStatement ps = null;
+		List<RoseBeans> roseList = null;
+
+		try {
+			con = DBManager.getConnection();
+
+			// SQL文を準備
+			String sql = ""
+					+"SELECT "
+					+"RoseID, "
+					+"RoseName, "
+					+"RosePicture, "
+					+"price "
+					+"FROM "
+					+"";//テーブル名考えんといかん
+
+
+			ps = con.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+
+			// 検索結果をbeanに格納する
+			while (rs.next()) {
+				RoseBeans rosebeans = new RoseBeans();
+				rosebeans.setRoseId(rs.getInt("RoseID"));
+				rosebeans.setRoseName(rs.getString("RoseName"));
+				rosebeans.setRosePicture(rs.getString("RosePicture"));
+				rosebeans.setRosePrice(rs.getInt("price"));
+				roseList.add(rosebeans);
+			}
+		} catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // DB切断処理
+            DBManager.close(ps, con);
+        }
+		return roseList;
+
+		}
+	public static RoseBeans selectOne() {
 
 		Connection con =null;
 		PreparedStatement ps = null;
@@ -19,7 +62,15 @@ public class RoseDao {
 			con = DBManager.getConnection();
 
 			// SQL文を準備
-			String sql = "";
+			String sql = ""
+					+"SELECT "
+					+"RoseID, "
+					+"RoseName, "
+					+"RosePicture, "
+					+"price "
+					+"FROM "
+					+"";//テーブル名考えんといかん
+
 
 			ps = con.prepareStatement(sql);
 
@@ -32,11 +83,6 @@ public class RoseDao {
 				rosebeans.setRoseName(rs.getString("RoseName"));
 				rosebeans.setRosePicture(rs.getString("RosePicture"));
 				rosebeans.setRosePrice(rs.getInt("price"));
-
-
-				//RoseBeans rb = null;
-
-				//接続とselect文とそれをRoseBeansに保持すると
 			}
 		} catch (SQLException e) {
             e.printStackTrace();
